a99de21178c9a18cfd0ebcd773e62d567ec3ac1e
Manav Sharma <manavsharma53@outlook.com>
2025-07-14 14:52:02 +0530

    ManavSharma Initial-commit

---
+++ StackOverflowProject.ViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z ]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
+
+
